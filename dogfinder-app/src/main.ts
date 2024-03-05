c17e4d34fc547202d0518b1abb4535e5f5880008
Ziad Arafat <ziada@nmsu.edu>
2024-03-05 16:07:35 -0700

    created a navbar component

--- dogfinder-app/src/main.ts
+++ dogfinder-app/src/main.ts
@@ -1,5 +1,3 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeView from 'primevue/config'
@@ -10,15 +8,21 @@ import router from './router'
 
 import Dropdown from 'primevue/dropdown';
 import Button from 'primevue/button';
+import Sidebar from 'primevue/sidebar';
+import PanelMenu from 'primevue/panelmenu'
+
 
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
+import "primeflex/primeflex.css";
 
 const app = createApp(App)
 
 app.component('Dropdown', Dropdown);
 app.component('Button', Button);
+app.component('Sidebar', Sidebar);
+app.component('PanelMenu', PanelMenu);
 
 app.use(PrimeView)
 app.use(createPinia())
