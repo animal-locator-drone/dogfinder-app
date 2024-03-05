2356473b888201c9a270c72b17cf3bad213dfa91
Ziad Arafat <ziada@nmsu.edu>
2024-03-05 15:24:27 -0700

    import PrimeView into the project and some components for a test page.

--- dogfinder-app/src/main.ts
+++ dogfinder-app/src/main.ts
@@ -2,12 +2,25 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeView from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
 
+
+import Dropdown from 'primevue/dropdown';
+import Button from 'primevue/button';
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
 const app = createApp(App)
 
+app.component('Dropdown', Dropdown);
+app.component('Button', Button);
+
+app.use(PrimeView)
 app.use(createPinia())
 app.use(router)
 
