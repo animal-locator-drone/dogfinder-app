209a87484a603b925632a318e09f0cdd169ad840
Ziad Arafat <ziada@nmsu.edu>
2024-03-07 05:05:19 -0700

    Fixed a lot of CSS problems and made the map and video stream fit to some extent

--- dogfinder-app/src/components/VideoStream.vue
+++ dogfinder-app/src/components/VideoStream.vue
@@ -1,17 +1,14 @@
 <template>
-        <video id="video" class="video-stream" autoplay controls>
-            <source src="https://i.imgur.com/JJLT4BD.mp4" type="video/mp4">
-            Your browser does not support the video tag.
+        <video id="video" autoplay controls  width="100%" height="85%">
+                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+                        type="video/mp4">
+                Your browser does not support the video tag.
         </video>
-    </template>
-    
-    <script setup lang="ts">
-    </script>
-    
-    <style scoped>
-    .video-stream {
-        position: relative;
-        width: 100%;
-        height: 100%;
-    }
-    </style>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
