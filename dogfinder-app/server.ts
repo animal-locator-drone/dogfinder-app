de33e58208a370fcfdb73a379c17f4dcc13c8992
Ziad Arafat <ziada@nmsu.edu>
2024-03-17 22:40:18 -0600

    added interface definitions for return values.

--- dogfinder-app/server.ts
+++ dogfinder-app/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import ViteExpress from 'vite-express';
 
-ViteExpress.config({mode: 'development'});
+ViteExpress.config({ mode: 'development' });
 
 const app = express();
 const PORT = 3000;
@@ -10,49 +10,69 @@ const PORT = 3000;
 app.use(express.json());
 
 // Routes
+interface Mission {
+        id: string;
+        name: string;
+        path_preview: string;
+}
 
 app.get('/missions_available', (req: Request, res: Response) => {
-    res.json({
-        missions: [
-            { id: "123", name: "Path A", path_preview: "TODO" }
-        ]
-    });
+        const missions: Mission[] = [
+                { id: "123", name: "Path A", path_preview: "TODO" }
+        ];
+        res.json({missions});
 });
+
 
 app.post('/select_mission', (req: Request, res: Response) => {
-    res.json({ status: "Mission Started" });
+        res.json({ status: "Mission Started" });
 });
+
+interface StatusInfo {
+        battery_percent: number;
+        time_elapsed: number;
+        current_location: [number, number];
+        time_remaining: number;
+        progress_percent: number;
+}
 
 app.get('/mission_status', (req: Request, res: Response) => {
-    res.json({
-        status_info: {
-            battery_percent: 69,
-            time_elapsed: 200,
-            current_location: [420.69, 69.420],
-            time_remaining: 500,
-            progress_percent: 69,
-        }
-    });
+        const status_info: StatusInfo = {
+                battery_percent: 69,
+                time_elapsed: 200,
+                current_location: [420.69, 69.420],
+                time_remaining: 500,
+                progress_percent: 69,
+        };
+
+        res.json({ status_info });
 });
 
+interface Detection {
+        id: string;
+        location: [number, number];
+        time: string;
+        images: string[];
+}
+
 app.get('/detections', (req: Request, res: Response) => {
-    res.json({
-        detections: [
-            {
-                id: "1234",
-                location: [123, 123],
-                time: "timeUTC",
-                images: [
-                    "www.link.to/image.png",
-                    "www.link.to/another_image.png"
-                ],
-            }
-        ]
-    });
+
+        const detections: Detection[] = [
+                {
+                        id: "1234",
+                        location: [123, 123],
+                        time: "timeUTC",
+                        images: [
+                                "www.link.to/image.png",
+                                "www.link.to/another_image.png"
+                        ],
+                }
+        ];
+        res.json({ detections });
 });
 
 app.post('/abort_mission', (req: Request, res: Response) => {
-    res.json({ status: "success" });
+        res.json({ status: "success" });
 });
 
 ViteExpress.listen(app, PORT, () => {
