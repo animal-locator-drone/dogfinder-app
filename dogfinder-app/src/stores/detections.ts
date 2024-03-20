4244c941bc24af790d9553791bee01a65eb0336b
Ziad Arafat <ziada@nmsu.edu>
2024-03-20 16:36:59 -0600

    defined the detections store

--- dogfinder-app/src/stores/detections.ts
+++ dogfinder-app/src/stores/detections.ts
@@ -1,12 +1,62 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { id } from 'vuetify/locale';
 
-export const useCounterStore = defineStore('counter', () => {
-        const count = ref(0)
-        const doubleCount = computed(() => count.value * 2)
-        function increment() {
-                count.value++
+interface State {
+        detections: Detection[];
+        selected_detection: Detection | null;
+
+}
+
+export const use_detections_store = defineStore('detections', {
+        state: (): State => {
+                return {
+                        detections: [],
+                        selected_detection: null
+                }
+        },
+
+        getters: {
+                detections(): Detection[] {
+                        return this.detections;
+                },
+                selected_detection(): Detection | null {
+                        return this.selected_detection;
+                },
+                detection_by_id(): FnDetectionById {
+                        return (id: string) => {
+                                return this.detections.find(
+                                        detection => detection.id === id
+                                ) || null;
+                        }
+                }
+        },
+
+        actions: {
+                async fetch_detections() {
+                        const response = await fetch('/detections');
+                        const data = await response.json();
+                        this.detections = data.detections;
+                },
+
+                async select_detection(id: string) {
+                        this.selected_detection = this.detection_by_id(id);
+                },
+
+                async new_detection(detection: Detection) {
+                        this.detections.push(detection);
+                }
+        
         }
-
-        return { count, doubleCount, increment }
 })
+
+interface Detection {
+        id: string;
+        location: [number, number];
+        time: string;
+        images: string[];
+}
+
+type FnDetectionById = {
+        (id: string): Detection | null;
+}
