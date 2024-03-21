1b0f5c1ea07909fcd5fa64ff69bec270d935b79c
Ziad Arafat <ziada@nmsu.edu>
2024-03-20 18:44:41 -0600

    Dog finder now lets you select dogs and displays the selected dog correctly.

--- dogfinder-app/src/components/DogFinder.vue
+++ dogfinder-app/src/components/DogFinder.vue
@@ -1,30 +1,15 @@
 <template>
-        <div class="dogfinder">
-                <div class="dogcard">
-                        <!-- <Card style="height: 15rem;">
-                                <template #title>Dog Found!</template>
-<template #content>
-                                        <img src="https://www.zbrushcentral.com/uploads/default/original/4X/3/2/d/32d485c620d70a154f0419d526e33299ab809fd2.jpeg"
-                                                alt="Dog" style="width: 30%; height: 50%;">
-                                        <p class="m-0">
-                                                
-                                        </p>
-                                        <Button @click="dog_found" severity="success" class="mt-2" label="Yes" />
-                                        <Button @click="dismiss_dog" severity="danger" class="mt-2" label="No" />
-                                </template>
-</Card> -->
+        <div v-if="!get_loading" class="flex align-items-center justify-content-center dogcard">
                         <div class="card">
                                 <div class="flex flex-column md:flex-row">
-                                        <div
-                                                class="w-full md:w-5 flex flex-column align-items-center justify-content-center gap-3 py-5">
-                                                <img src="https://www.zbrushcentral.com/uploads/default/original/4X/3/2/d/32d485c620d70a154f0419d526e33299ab809fd2.jpeg"
-                                                alt="Dog" style="width: 100%; height: 100%;">
-                                                
+                                        <div class="w-full md:w-5">
+                                                <DogGallery :detection="detections_store.get_detection_by_id(get_selected_detection)" />
                                         </div>
                                         <div class="w-full md:w-2">
-                                                <Divider layout="vertical" class="hidden md:flex"><b>Is this your dog?</b></Divider>
-                                                <Divider layout="horizontal" class="flex md:hidden" align="center">
-                                                        <b>Is this your dog?</b></Divider>
+                                                <Divider layout="vertical" class="hidden md:flex">
+                                                        <b>Is this your dog?</b>
+                                                </Divider>
+
                                         </div>
                                         <div class="w-full md:w-5 flex align-items-center justify-content-center py-5">
                                                 <Button label="Yes" icon="pi pi-check" severity="success"
@@ -34,17 +19,17 @@
                                         </div>
                                 </div>
                         </div>
-                </div>
                 <div class="doggrid">
-                        <VirtualScroller :items="dog_detections" :itemSize="30" orientation="horizontal"
+                        <VirtualScroller :items="get_detections" :itemSize="30" orientation="horizontal"
                                 class="border-1 surface-border border-round" style="width: 40rem; height: 10rem;"
                                 :pt="{ content: 'flex flex-row' }">
                                 <template v-slot:item="{ item, options }">
                                         <div :class="['align-items-center p-2', { 'surface-hover': options.odd }]"
                                                 style="width: 10rem;">
-                                                <img :src="item.img_src" alt="Dog" style="width: 100%; height: 50%;">
-                                                <p class="m-0">{{ item.name }}</p>
 
+                                                <p class="m-0">{{ item.id }}</p>
+
+                                                <DogGallery :detection="item" />
                                         </div>
                                 </template>
                         </VirtualScroller>
@@ -53,50 +38,25 @@
 </template>
 
 <script setup lang="ts">
+import { use_detections_store } from '@/stores/detections';
+import { storeToRefs } from 'pinia';
+import DogGallery from '@/components/DogGallery.vue';
 
-import { computed } from 'vue'
+const detections_store = use_detections_store();
 
-// const dogFound = ref(false)
+const { 
+        get_detections,
+        get_selected_detection,
+        get_detection_by_id,
+        get_loading
+} = storeToRefs(detections_store);
 
-// const dog_found = () => {
-//         console.log('Dog found')
-//         dogFound.value = true
-// }
 
-// const dismiss_dog = () => {
-//         console.log('Dog dismissed')
-//         dogFound.value = false
-// }
 
-const dog_detections = computed(() => {
-        return [
-                {
-                        id: 1,
-                        name: 'Doggo 1',
-                        img_src: "https://www.zbrushcentral.com/uploads/default/original/4X/3/2/d/32d485c620d70a154f0419d526e33299ab809fd2.jpeg"
-                },
-                {
-                        id: 2,
-                        name: 'Doggo 2',
-                        img_src: "https://www.zbrushcentral.com/uploads/default/original/4X/3/2/d/32d485c620d70a154f0419d526e33299ab809fd2.jpeg"
-                },
-                {
-                        id: 3,
-                        name: 'Doggo 3',
-                        img_src: "https://www.zbrushcentral.com/uploads/default/original/4X/3/2/d/32d485c620d70a154f0419d526e33299ab809fd2.jpeg"
-                },
-                {
-                        id: 4,
-                        name: 'Doggo 4',
-                        img_src: "https://www.zbrushcentral.com/uploads/default/original/4X/3/2/d/32d485c620d70a154f0419d526e33299ab809fd2.jpeg"
-                },
-                {
-                        id: 5,
-                        name: 'Doggo 5',
-                        img_src: "https://www.zbrushcentral.com/uploads/default/original/4X/3/2/d/32d485c620d70a154f0419d526e33299ab809fd2.jpeg"
-                }
-        ]
-})
+console.log('detections:', get_detections);
+
+
+
 
 </script>
 
