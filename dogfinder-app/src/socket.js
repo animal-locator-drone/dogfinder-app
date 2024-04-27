d964f84e477924d082ac10be1eb0b3f8a7fdef42
Ziad Arafat <ziada@nmsu.edu>
2024-04-27 13:26:05 -0600

    socketio set URL with config and auto reconnect on disconnect.

--- dogfinder-app/src/socket.js
+++ dogfinder-app/src/socket.js
@@ -1,28 +1,29 @@
 import { reactive } from "vue";
 import { io } from "socket.io-client";
-import {use_detections_store} from './stores/detections'
+import { use_detections_store } from './stores/detections'
 
 export const state = reactive({
-  connected: false,
-  fooEvents: [],
-  barEvents: []
+        connected: false,
+        fooEvents: [],
+        barEvents: []
 });
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+const URL = process.env.NODE_ENV === "production" ? undefined : import.meta.env.VITE_SOCKET_URL;
 
 export const socket = io(URL);
 
 socket.on("connect", () => {
-  state.connected = true;
+        state.connected = true;
 });
 
 socket.on("disconnect", () => {
-  state.connected = false;
+        state.connected = false;
+        socket.connect();
 });
 
 socket.on("new_detection", (data) => {
-       
+
         const detections_store = use_detections_store()
 
         detections_store.new_detection(data)
