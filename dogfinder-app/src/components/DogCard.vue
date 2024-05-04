cba8882e181bcd0982c34fab44100eff7182f9d4
Ziad Arafat <ziada@nmsu.edu>
2024-05-03 22:23:09 -0600

    created dialog for tracking a dog.

--- dogfinder-app/src/components/DogCard.vue
+++ dogfinder-app/src/components/DogCard.vue
@@ -1,4 +1,8 @@
 <template>
+<TrackDog 
+        v-model:visible="track_dog_visible" 
+        :tracked_detection="get_selected_detection"
+/>
 <Toast />
 <ConfirmDialog group="dismiss_dog"></ConfirmDialog>
 <div
@@ -38,15 +42,20 @@
 </div>
 </template>
 <script setup lang="ts">
-import { use_detections_store } from '@/stores/detections';
+import { ref } from 'vue';
+import { use_detections_store, Detection } from '@/stores/detections';
 import { storeToRefs } from 'pinia';
 import { use_missions_store } from '@/stores/missions';
 
 import { useConfirm } from "primevue/useconfirm";
 import { useToast } from "primevue/usetoast";
 
+import TrackDog from '@/components/TrackDog.vue';
+
 const confirm = useConfirm();
 const toast = useToast();
+
+const track_dog_visible = ref(false)
 
 
 const detections_store = use_detections_store();
@@ -61,6 +70,7 @@ const {
 function yes_button_click(event: Event) {
         if (get_selected_detection.value) {
                 missions_store.track_dog(get_selected_detection.value.id);
+                track_dog_visible.value = true;
         }
 
 
---
+++ dogfinder-app/src/components/TrackDog.vue
@@ -0,0 +1,32 @@
+<template>
+        <div class="card flex justify-content-center">
+                <Dialog v-model:visible="visible" modal header="Tracking Dog" :style="{ width: '25rem' }">
+                        <span class="p-text-secondary block mb-5">Update your information.</span>
+                        <div
+                                class="flex align-items-center w-full justify-content-center h-full bg-primary font-bold border-round">
+                                <Image :image-class="'border-3 border-primary p-1 shadow-8 w-full max-h-30rem'"
+                                        v-if="props.tracked_detection" 
+                                        :src="props.tracked_detection.images[0]"
+                                        :preview="true" />
+                                <h3 v-else>Please Select One</h3>
+                        </div>
+                </Dialog>
+        </div>
+</template>
+
+<script setup lang="ts">
+import { ref, defineModel } from "vue";
+
+import { Detection } from '@/stores/detections';
+
+
+const props = defineProps<{
+        tracked_detection: Detection;
+}>();
+
+const visible = defineModel<boolean>('visible', {
+        required: true,
+        default: false
+});
+
+</script>
